a9fe46fa096d00e3338b275b131058f18a2bcca3
akj26 <akj26@fq-130-49-32-144.wireless.pitt.edu>
2017-10-16 14:40:37 -0400

    final

---
+++ BasicGit.java
@@ -0,0 +1,11 @@
+//Program to print numbers 1 -10
+public class BasicGit
+{
+    public static void main(String[] args)
+    {
+        for(int i = 1; i <= 10; i++)
+        {
+            System.out.println(i);
+        }
+    }
+}
